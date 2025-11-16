466722287e738975287f2355a53460b32d19764e
Grey Fundr <dev@greyfundr.com>
2025-11-16 00:44:26 -0800

    Use PORT for Render deployment

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
- // MongoDB Atlas Connection (now using env variable)
+// MongoDB Atlas Connection (use env variable)
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -18,8 +17,9 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
 
-
-
+// Routes
 app.use("/auth", authRoute);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// Required for Render !!
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
